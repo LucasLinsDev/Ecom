c71ba1e119691488150df9727f6585c575b02a32
Lucas Lins <lucaslinsperfil@gmail.com>
2022-09-25 23:52:01 -0300

    swiper products

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,8 +2,23 @@ import styles from './Banner.module.scss';
 
 import banner_lateral from '../../resource/banner/banner_lateral.png'
 
-export function Banner(){
+interface BannerProps{
+  banner?:string,
+  title?:string,
+  link?:string
+  alt?:string,
+}
+
+export function Banner({banner=banner_lateral,alt='default'}:BannerProps){
   return (
-    <div></div>
+    <div className={styles.banner}>
+      <div className={styles.image}>
+          <img src={banner} alt={alt}/>
+      </div>
+      <div className={styles.content}>
+        <h1>Desktops</h1>
+        <a>See All Products</a>
+      </div>
+    </div>
   )
 }
--- src/pages/Home/RowProducts/RowProducts.tsx
+++ src/pages/Home/RowProducts/RowProducts.tsx
@@ -1,7 +1,7 @@
 import styles from './RowProducts.module.scss';
 import { CartProduct } from '../../../components/Card/Product/CardProduct';
-import 'swiper/css/navigation'
-import 'swiper/css/thumbs'
+
+
 import {Swiper,SwiperSlide} from 'swiper/react';
 import {Navigation,Thumbs } from 'swiper';
 import { useRef } from 'react';
@@ -19,54 +19,52 @@ export function RowProducts(){
     <>
       <div className={styles.header}></div>
       <div className={styles.row}>
-        <div className={styles.left}>
-          <Banner/>
-        </div>
-        <div className={styles.right}>
-
-             <Swiper 
-                onInit={(swiper:any) => {
-                  swiper.params.navigation.prevEl = preRef.current;
-                  swiper.params.navigation.nextEl = nextRef.current;
-                  swiper.navigation.init();
-                  swiper.navigation.update();
-                }}
-                loop={true}
-                modules={[Navigation,Thumbs]}
-                grabCursor={true}
-                breakpoints={{
-                    320: {
-                      width: 320,
-                      slidesPerView: 1.8,
-                    },
-                    400: {
-                      width: 400,
-                      slidesPerView: 2,
-                    },
-              
-                    768: {
-                      width: 768,
-                      slidesPerView: 4,
-                    },
-                    980: {
-                      width: 980,
-                      slidesPerView:4,
-                    },
-                    1400: {
-                      width: 1400,
-                      slidesPerView:6
-                    }      
-              }}>
-                {[1,2,3,4,5,6,7].map(()=>(
-                     <SwiperSlide>
-                        <CartProduct title='PS5' price='999.99' image={ps5} />
-                      </SwiperSlide>
-                ))}
-             </Swiper>
-            <div ref={preRef}>  <img src={arrow}/>    </div>
-            <div ref={nextRef}> <img src={arrow}/>    </div>
-      
-        </div>
+        
+              <Banner/>
+       
+    
+              <Swiper 
+                  onInit={(swiper:any) => {
+                    swiper.params.navigation.prevEl = preRef.current;
+                    swiper.params.navigation.nextEl = nextRef.current;
+                    swiper.navigation.init();
+                    swiper.navigation.update();
+                  }}
+                  loop={true}
+                  className={styles.swiper_wrapper}
+                  modules={[Navigation,Thumbs]}
+                  grabCursor={true}
+                  breakpoints={{
+                     320: {
+                  width: 320,
+                  slidesPerView: 1.8,
+                },
+                400: {
+                  width: 400,
+                  slidesPerView: 2,
+                },
+           
+                768: {
+                  width: 768,
+                  slidesPerView: 5,
+                },
+                 1400: {
+                  width: 1400,
+                  slidesPerView:6,
+                },
+                }}>
+                
+                  {[1,2,3,4,5,6,7,8,9,10].map(()=>(
+                      <SwiperSlide>
+                          <CartProduct title='PS5' price='999.99' image={ps5} />
+                        </SwiperSlide>
+                  ))}
+                   <div ref={preRef}>  <img src={arrow}/>    </div>
+                   <div ref={nextRef}> <img src={arrow}/>    </div>
+        
+              </Swiper>
+            
+     
       </div>
     </>
     )
