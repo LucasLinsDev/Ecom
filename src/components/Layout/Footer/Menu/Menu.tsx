96b9c2f2e27ffc7664861951a2d17e62e008ed0f
Lucas Lins <lucaslinsperfil@gmail.com>
2022-09-26 11:41:04 -0300

    add footer ok

--- src/components/Layout/Footer/Menu/Menu.tsx
+++ src/components/Layout/Footer/Menu/Menu.tsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import styles from './Menu.module.scss';
 
+import { HiChevronUp, HiOutlineChevronDown } from 'react-icons/hi';
 export function Menu (){
+ const [show,setShow]=useState(false);
+
+  const menu=useMediaQuery({query: '(min-width: 800px)' })
+  
+
+  useEffect(()=>{
+    {menu ? setShow(true) : setShow(false)}
+  },[menu])
+
+  const handlerShow = ()=>{
+  
+    if(menu === false){
+         setShow(!show);
+    }
+   
+  }
+
   return(
     <div className={styles.footer_menu}>
       <div className={styles.menu}>
-        <div className={styles.menushow}>
-          <p>Laptops</p>
+        <div className={styles.menushow} onClick={handlerShow}>
+          <p>Laptops</p> {!menu && (
+            !show ? <HiOutlineChevronDown size={24} color='white'/> : <HiChevronUp size={24} color='white'/>
+          )}
         </div>
-         <ul>
+        {show && (
+             <ul>
                   <li>About Us</li>
                   <li>About Zip</li>
                   <li>Privacy Policy</li>
@@ -19,6 +42,7 @@ export function Menu (){
                   <li>Advanced Search</li>
                   <li>Newsletter Subscription</li>
           </ul>
+        )}
       </div>
     </div>
   )
